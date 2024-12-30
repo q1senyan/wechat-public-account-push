f298942c732e273e0aeaf18e6590bc797b6492af
q1senyan <146896309+q1senyan@users.noreply.github.com>
2024-12-30 20:26:06 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,9 +6,7 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-  // 请将以下代码按照图示粘贴到对应位置
-  USE_PASSAGE: 'push-deer',
-  
+
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx20261f8628225790',
 
@@ -25,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOTRG7AWH2FB-dHKaXPYmXURUSMs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'w7Z60a0stqnVVslaKYsUqXaaaN9m-M8ZdaBfBPT47ss',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -53,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'kfRdWX12rCKtlDNjcTKYP-0w0N_QRhj8IpuC_cEOS-w',
 
   CALLBACK_USERS: [
     {
