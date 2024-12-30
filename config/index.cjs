a6454a6835ccc15b3e14541fb119f0dee973b9c9
q1senyan <146896309+q1senyan@users.noreply.github.com>
2024-12-30 21:49:20 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOTRG7AWH2FB-dHKaXPYmXURUSMs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'mqOMvRLphT9Ga6SPBTkSqXuw6W32jG-OuOncKxEfuCM',
+      useTemplateId: 'Cj5Mv1HZ4hvvZ_LiH_oBFDeMMtKmUTq4zbJH4Z0hWeI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-09',
       festivals: [
