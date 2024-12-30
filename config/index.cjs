8a3ec30742b57b9178383d752e38a77c7d5d1161
q1senyan <146896309+q1senyan@users.noreply.github.com>
2024-12-30 03:59:35 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,62 +6,63 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-deer',
+  
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx20261f8628225790',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '53dbd73dfd00de720de917bb9ed37a03',
+  APP_SECRET: '',
 
-  // PROVINCE: '上海',
-  CITY: '上海',
+  PROVINCE: '广东',
+  CITY: '惠州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '超级贝贝😘',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOTRG7AWH2FB-dHKaXPYmXURUSMs',
+      id: 'PDU33373THM6R9pquWQKtjf8df71m8vTL5qFUTSap',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'w7Z60a0stqnVVslaKYsUqXaaaN9m-M8ZdaBfBPT47ss',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '05-09',
+      horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        // {
-        //   type: '*生日', name: '贝贝', year: '2004', date: '09-09',
-        // },
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '贝贝宝宝', year: '2004', date: '05-09',
+          type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2024', date: '10-07',
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2024-12-08' },
-        // // 结婚纪念日
-        // { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'kfRdWX12rCKtlDNjcTKYP-0w0N_QRhj8IpuC_cEOS-w',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOTRG7AWH2FB-dHKaXPYmXURUSMs',
+      id: '',
     }
   ],
 
 }
 
 module.exports = USER_CONFIG
-
